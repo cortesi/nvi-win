2e123962f00c1c0b2f54c73d13d0233e513491eb
Aldo Cortesi <aldo@corte.si>
2025-01-02 13:55:06 +1300

    Basic implementation with panes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use anyhow::{anyhow, Result};
 use nvi::{
     input, lua_exec,
     nvi_macros::*,
-    nvim::types::{Border, Relative, TabPage, Window, WindowConf},
+    nvim::types::{TabPage, Window},
+    ui::pane,
 };
 
 use tracing::trace;
@@ -12,83 +13,48 @@ mod demos;
 #[cfg(test)]
 mod tests;
 
-const DEFAULT_HINT_HL: &str = "Bold";
-const DEFAULT_NORMAL_HL: &str = "Normal";
-const DEFAULT_KEYS: &str = "asdfghjklqwertyuiopzxcvbnm";
-const FLOAT_WIDTH: i64 = 6;
-const FLOAT_HEIGHT: i64 = 3;
+const DEFAULT_KEYS: &[&str] = &[
+    "a", "s", "d", "f", "g", "h", "j", "k", "l", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+    "z", "x", "c", "v", "b", "n", "m",
+];
+
+const FLOAT_WIDTH: usize = 7;
+const FLOAT_HEIGHT: usize = 3;
 
 #[derive(Clone)]
 struct NviWin {
-    keys: String,
+    keys: Vec<String>,
+    panes: Vec<pane::Pane>,
 }
 
 #[nvi_plugin]
 impl NviWin {
     fn new() -> Self {
         NviWin {
-            keys: DEFAULT_KEYS.to_string(),
+            keys: DEFAULT_KEYS.iter().map(|s| s.to_string()).collect(),
+            panes: vec![],
         }
     }
 
-    async fn show_hints(&self, client: &mut nvi::Client, windows: Vec<Window>) -> Result<()> {
+    async fn show_hints(&mut self, client: &mut nvi::Client, windows: &[Window]) -> Result<()> {
         for (i, w) in windows.iter().enumerate() {
-            let key = self.keys.chars().nth(i).unwrap();
-            let buffer = client.nvim.create_buf(false, true).await?;
-            if u64::from(buffer.clone()) == 0 {
-                return Err(anyhow!("failed to create buffer"));
-            }
-            client
-                .nvim
-                .buf_set_lines(
-                    &buffer,
-                    0,
-                    -1,
-                    false,
-                    vec![
-                        "  ".to_string(),
-                        format!("  {}", key.to_string()),
-                        "  ".to_string(),
-                    ],
+            let key = self.keys[i].clone();
+
+            let pane = pane::Pane::builder()
+                .with_win_pos(w.clone(), pane::Pos::Center, 0)
+                .build(
+                    client,
+                    pane::Content::center(FLOAT_WIDTH, FLOAT_HEIGHT, &key.to_string()),
                 )
                 .await?;
-            client
-                .nvim
-                .buf_add_highlight(&buffer, 0, DEFAULT_HINT_HL, 1, 0, -1)
-                .await?;
-
-            let width = client.nvim.win_get_width(w).await?;
-            let height = client.nvim.win_get_height(w).await?;
-            let row = (height - FLOAT_HEIGHT) / 2 - 1;
-            let col = (width - FLOAT_WIDTH) / 2 - FLOAT_WIDTH;
-
-            let float_win = client
-                .nvim
-                .open_win(
-                    &buffer,
-                    true,
-                    WindowConf::default()
-                        .relative(Relative::Win)
-                        .win(w.clone())
-                        .row(row)
-                        .col(col)
-                        .width(FLOAT_WIDTH as u64)
-                        .height(FLOAT_HEIGHT as u64)
-                        .style("minimal".to_string())
-                        .border(Border::Single)
-                        .noautocmd(true)
-                        .focusable(false),
-                )
-                .await?;
-            // float_win
-            //     .set(client, "winhl", format!("Normal:{}", DEFAULT_NORMAL_HL))
-            //     .await?;
-            float_win.set(client, "diff", false).await?;
+            self.panes.push(pane);
         }
         lua_exec!(client, "vim.cmd('redraw')").await?;
         Ok(())
     }
 
+    /// Get the list of windows we need to choose from. Exclude the current window and
+    /// floating windows.
     async fn windows(&self, client: &mut nvi::Client) -> Result<Vec<Window>> {
         let current = client.nvim.get_current_win().await?;
         let mut ret = vec![];
@@ -108,30 +74,30 @@ impl NviWin {
     /// Pick a window, and return the window ID. If there's only one window, return that window
     /// immediately. Otherwise, display an overlay and ask the user for input.
     #[request]
-    async fn pick(&self, client: &mut nvi::Client) -> Result<Window> {
+    async fn pick(&mut self, client: &mut nvi::Client) -> Result<Window> {
         let windows = self.windows(client).await?;
-        self.show_hints(client, windows.clone()).await?;
+        self.show_hints(client, &windows).await?;
         let c = input::get_keypress(client).await?;
-        let offset = self.keys.find(&c.key.to_string()).unwrap_or(0);
-        if offset >= windows.len() {
-            return Err(anyhow!("invalid window"));
+
+        while let Some(p) = self.panes.pop() {
+            p.destroy().await?;
         }
-        Ok(windows[offset].clone())
+
+        if let Some(offset) = self.keys.iter().position(|x| **x == c.key.name()) {
+            if offset >= windows.len() {
+                return Err(anyhow!("invalid window"));
+            }
+            Ok(windows[offset].clone())
+        } else {
+            return Ok(client.nvim.get_current_win().await?);
+        }
     }
 
     /// Pick a window and jump to it.
     #[request]
-    async fn jump(&self, client: &mut nvi::Client) -> Result<()> {
+    async fn jump(&mut self, client: &mut nvi::Client) -> Result<()> {
         let window = self.pick(client).await?;
-        client
-            .info(&format!("jumping to window {}", window))
-            .await?;
         client.nvim.set_current_win(&window).await?;
-        Ok(())
-    }
-
-    async fn connected(&mut self, _client: &mut nvi::Client) -> nvi::error::Result<()> {
-        trace!("nvi_win connected");
         Ok(())
     }
 }
